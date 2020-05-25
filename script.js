0a7af86d56ee2ef0219c6b9671d7474474f23180
baryska <55710842+baryska@users.noreply.github.com>
2020-05-25 15:01:04 +0200

    trying to run addEventListener

---
+++ script.js
@@ -0,0 +1,42 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li")
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function addClassAfterClick() {
+	this.classList.toggle('done')
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+function doneEvent() {
+	for (i = 0; i < li.length; i++) {
+		li[i].addEventListener('click', addClassAfterClick)
+	}
+}
+
